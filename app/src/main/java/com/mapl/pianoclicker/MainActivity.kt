17258908a62e5b19ce6e2b18d69fd448208af374
Matthew Plokhov <maplchannel@yandex.ru>
2021-11-04 17:32:52 +0300

    Init commit

---
+++ app/src/main/java/com/mapl/pianoclicker/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mapl.pianoclicker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapl.pianoclicker.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/mapl/pianoclicker/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.mapl.pianoclicker.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
